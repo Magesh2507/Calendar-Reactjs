a444484d51e720306b7c7fefaace15b1f355cc27
Magesh V <147228347+Magesh2507@users.noreply.github.com>
2024-04-05 13:12:24 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 
 import './App.css'
-import WeatherApp from './components/weather-app/WeatherApp'
+//import WeatherApp from './components/weather-app/WeatherApp'
 //import GetAdvice from './components/get-advice/GetAdvice'
-import QrCode from './components/qr-code-generator/QrCode'
+//import QrCode from './components/qr-code-generator/QrCode'
 import Calendar from './components/calendar/Calendar'
 // import UserCard from './assets/UserCard/UserCard'
 
